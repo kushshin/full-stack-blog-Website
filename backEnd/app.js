44e5e578cd65fbb838f85f045a502bb9811ff033
kushshin <kshitij6388@gmail.com>
2025-09-27 18:26:49 +0530

    CRUD updated

--- backEnd/app.js
+++ backEnd/app.js
@@ -15,7 +15,6 @@ const PORT = process.env.PORT || 3000
 
 app.use(cors(
     { origin: "https://full-stack-blog-website-eta.vercel.app",
-         methods: ["GET","POST","PUT","PATCH","DELETE"],
     // { origin: true,
         credentials: true
     }))
